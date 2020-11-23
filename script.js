fe0551d3708cecb16b256bff5331a468d01308ff
Florian SIMUNEK <florian.simunek@gmail.com>
2020-11-23 23:10:00 +0100

    Added color for counter value

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ const plusBtn = document.getElementById('plus-button');
 
 minusBtn.addEventListener('click', function(){
     counter--;
-    counterValueText.textContent = counter; 
+    counterValueText.textContent = counter;
+    counterColor();
 })
 
 plusBtn.addEventListener('click', function(){
     counter++;
-    counterValueText.textContent = counter;    
+    counterValueText.textContent = counter;
+    counterColor();
 })
 
+function counterColor(){
+    if(counter > 0){
+        counterValueText.style.color = 'green';
+    } else if(counter < 0 ){
+        counterValueText.style.color = 'red';
+    } else {
+        counterValueText.style.color = '#333';
+    }
+}
